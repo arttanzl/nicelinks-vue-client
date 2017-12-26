6c9c5e5dddb439dd88e5b8e0bbb3b02c6297f782
nicejade <jeffygisgreat@gmail.com>
2017-12-26 22:13:46 +0800

    feat(Index): extraction component form Index page

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -26,90 +26,40 @@
       </section>
     </section>
   </section>
-  <div class="countup-area">
-    <h2 class="medium-font">{{ $t('countupText') }}</h2>
-    <CountUp
-      class="countup-number"
-      :start="0"
-      :end="theDisplayCount"
-      :decimals="0"
-      :duration="2.5"
-      :options="countUpoptions"
-      @callback="onCountUpCallback">
-    </CountUp>
-  </div>
+  <LinkCountup></LinkCountup>
 </div>
 </template>
 
 <script>
 import './../assets/css/flower.css'
-import CountUp from 'components/CountUp'
+import LinkCountup from 'components/homepage/LinkCountup'
 
 export default {
   name: 'Index',
 
   data () {
     return {
-      isMobile: window.innerWidth <= 960,
-      theDisplayCount: 0,
-      totalLinksCount: 0,
-      countUpoptions: {
-        useEasing: true,
-        useGrouping: true,
-        separator: ',',
-        decimal: '.',
-        prefix: '',
-        suffix: ''
-      }
     }
   },
 
   components: {
-    CountUp
+    LinkCountup
   },
 
   created () {
-    let params = {active: true}
-    this.$apis.getAllLinksCount(params).then(result => {
-      this.totalLinksCount = result
-      this.handleDisplayCount()
-    }).catch((error) => {
-      this.totalLinksCount = 99
-      this.handleDisplayCount()
-      console.log(error)
-    })
   },
 
   methods: {
-    handleDisplayCount () {
-      if (this.isMobile) {
-        this.theDisplayCount = this.totalLinksCount
-      } else {
-        window.addEventListener('scroll', this.handleScroll)
-      }
-    },
-
-    handleScroll () {
-      if (window.scrollY >= 188 && window.scrollY <= 999) {
-        this.theDisplayCount = this.totalLinksCount
-      }
-    },
-    /* -----------------------on***Event----------------------- */
-    onCountUpCallback: (ins) => {
-      // ins.update(ins.endVal + 100)
-    }
   },
 
   locales: {
     en: {
       exploreNice: 'Explore Nice',
-      niceLinksDesc: 'Collection of excellent web sites around the world so that people can see and learn more interesting and useful things',
-      countupText: 'The number of high quality websites has been included'
+      niceLinksDesc: 'Collection of excellent web sites around the world so that people can see and learn more interesting and useful things'
     },
     zh: {
       exploreNice: '发现美好',
-      niceLinksDesc: '云集全球优秀网站，让人们可以从中，见识和学习更多有趣且有用的事物',
-      countupText: '已经收录优质网站个数'
+      niceLinksDesc: '云集全球优秀网站，让人们可以从中，见识和学习更多有趣且有用的事物'
     }
   }
 }
@@ -125,7 +75,6 @@ export default {
     margin-left: 0
   }
 }
-.lotus{}
 
 .flower-container h1 {
   font-weight: bold;
@@ -180,9 +129,22 @@ export default {
   }
 }
 
-@media (max-width: 390px) {
+@media (max-width: 414px) {
   .flower-container .flower {
-    font-size: 12px
+    font-size: 1.6rem !important;
+  }
+  .flower-container .flower p {
+    font-size: 1.8rem;
+    padding: 0px 10px;
+    line-height: 1.8rem;
+    left: -4em;
+    right: -4em;
+  }
+}
+
+@media (max-width: 375px) {
+  .flower-container .flower {
+    font-size: 1.3rem !important;
   }
 }
 
@@ -209,18 +171,5 @@ export default {
 }
 .animated.hinge {
   animation-duration: 6s;
-}
-
-.countup-area{
-  .countup-number{
-    display: block;
-  }
-  color: #000;
-  width: 100%;
-  height: 21.99rem;
-  @include flex-box-center(column);
-  background: #8e9eab; /* fallback for old browsers */
-  background: -webkit-linear-gradient(to top, #8e9eab, #eef2f3); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to top, #8e9eab, #eef2f3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 </style>
